2db328970dd0686bde03a53bdfd437382db4fc22
chuyuxuan123 <chuyuxuan@126.com>
2019-04-29 20:11:19 +0800

    update

--- web_v2/front/src/component/Cart.js
+++ web_v2/front/src/component/Cart.js
@@ -156,18 +156,23 @@ export default class Cart extends Component {
       }
       console.log(response);
     }).catch((error)=>{
-      console.log(error);
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
     })
-
-
-    // Axios.get("http://localhost:8080/orders/cart/buy").then((response)=>{
-    //   if(response.data==200){
-    //     message.info("购买成功");
-    //     this.setState({dataSource:[],
-    //                   totalPrice:0});
-    //     // window.history.pushState({},'',"/booklist");
-    //   }
-    // })
   }
 
   render() {
