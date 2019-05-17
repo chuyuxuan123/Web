330dfc00a919bdeac21c86c11bd1b376c871220b
chuyuxuan123 <chuyuxuan@126.com>
2019-05-17 19:20:34 +0800

    图片上传

--- web_v2/back/src/main/java/com/example/demo/controller/ImageController.java
+++ web_v2/back/src/main/java/com/example/demo/controller/ImageController.java
@@ -4,6 +4,7 @@ package com.example.demo.controller;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -26,5 +27,25 @@ public class ImageController {
     }
 
 //    @PostMapping
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "上传失败，请选择文件";
+        }
+
+        String fileName = file.getOriginalFilename();
+//        System.out.println(fileName);
+        String filePath = "G:\\Web\\proj\\web_v2\\back\\src\\main\\resources\\img\\";
+        File dest = new File(filePath + fileName);
+        try {
+            file.transferTo(dest);
+            return "上传成功";
+        } catch (IOException e) {
+        }
+        return "上传失败！";
+    }
+
 
 }
